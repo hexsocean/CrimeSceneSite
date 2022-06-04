9c038c00886a67a039fdd8c71d8082e82ab5ecc9
Hex <lumengeared@gmail.com>
2022-06-04 20:34:26 +0800

    bare bones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  buildOptions: {
-    site: "https://astro-minimal-starter.netlify.app/", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true, // Generate sitemap (set to "false" to disable)
-  },
-  // Comment out "renderers: []" to enable Astro's default component support.
-  renderers: ["@astrojs/renderer-preact"],
+import { defineConfig } from 'astro/config';
+import solid from '@astrojs/solid-js';
+
+// https://astro.build/config
+export default defineConfig({
+	// Enable Solid to support Solid JSX components.
+	integrations: [solid()],
 });
