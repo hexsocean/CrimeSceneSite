4c85f74e09f41446868d86df112c73165d03212d
sen <lumengeared@gmail.com>
2022-06-09 17:34:56 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import solid from '@astrojs/solid-js';
 // https://astro.build/config
 export default defineConfig({
 	// Enable Solid to support Solid JSX components.
+	site: "https://hexsocean.github.io",
+	base: "year9-t2-innovations-crime-scene-site",
 	integrations: [solid()],
 });
