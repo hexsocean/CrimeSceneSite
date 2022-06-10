696e8fa15982330665d98ffb95eb06e0ac644aec
socean <lumengeared@gmail.com>
2022-06-10 18:07:19 +0800

    astro configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig, searchForWorkspaceRoot } from 'astro/config';
-import solid from '@astrojs/solid-js';
+import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable Solid to support Solid JSX components.
-	site: "https://hexsocean.github.io",
-	base: "year9-t2-innovations-crime-scene-site",
-	integrations: [solid()],
-	server: {
-		fs: {
-		// Allow serving files from one level up to the project root
-		allow: [searchForWorkspaceRoot(process.cwd())]
-		}
-	}
+  // Enable Solid to support Solid JSX components.
+  site: "https://hexsocean.github.io",
+  base: "year9-t2-innovations-crime-scene-site",
+  integrations: [solid()],
+  // server: {
+  // 	fs: {
+  // 	// Allow serving files from one level up to the project root
+  // 	allow: [searchForWorkspaceRoot(process.cwd())]
+  // 	}
+  // }
 });
